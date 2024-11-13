5fe2cd01a10308ce2f9427927b93261338fd836d
Diana Louise <dianaluise24@gmail.com>
2024-11-13 19:47:01 -0300

    Atualização Tela Cadastro e Login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -10,6 +10,26 @@ export default function Login() {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
+  const logar = async () => {
+    if (!email || !senha) {
+      Alert.alert("Erro! Por favor, preencha ambos os campos.");
+      return;
+    }
+
+    try {
+      const response = await axios.post('http://10.0.2.2:3000/login', { email, senha });
+      
+      if (response.status === 200) {
+        // Sucesso no login
+        Alert.alert('Login realizado com sucesso!');
+        // Aqui você pode redirecionar para a tela principal ou para onde deseja
+      } else {
+        Alert.alert('Email ou senha incorretos!');
+      }
+    } catch (error) {
+      Alert.alert('Erro na conexão com o servidor!');
+    }
+  };
 
   return (
     <View>
